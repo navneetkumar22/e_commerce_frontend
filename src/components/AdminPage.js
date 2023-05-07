6ad6c83002e3c7b00ae300a5d3578ece7acda8a6
Navneet Kumar <navneetsinghpanwar90@gmail.com>
2023-05-07 19:09:30 +0530

    added admin page

---
+++ src/components/AdminPage.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import "../styles/AdminPage.css";
+import { Link } from "react-router-dom";
+import forkImage from "../assets/forkImage.png";
+import AdminProfile from "../components/AdminProfile";
+import AdminUsers from "../components/AdminUsers";
+import AdminCollections from "../components/AdminCollections";
+import AdminProducts from "../components/AdminProducts";
+import AdminOrders from "../components/AdminOrders";
+
+
+const AdminPage = () => {
+
+    const [clickedBtn, setCickedBtn] = useState(1);
+
+
+    return (
+        <>
+            {/* navigation section  */}
+            <section className='nav'>
+                <div className='store'>
+                    <img src={forkImage} alt='logo' />
+                    <h3>FOOD <br /> STORE</h3>
+                </div>
+                <div className='nav-div'>
+                    <ul>
+                        <Link to="/"><li>HOME</li></Link>
+                        <Link to="/admin"><li>PROFILE</li></Link>
+                        <Link to="/products"><li>PRODUCTS</li></Link>
+                        <Link to="/about"><li>ABOUT US</li></Link>
+                        <Link to="/contact"><li>CONTACT US</li></Link>
+                    </ul>
+                </div>
+                <div className='extra-div'></div>
+            </section>
+
+            <div className='admin-panel'>
+                Dashboard
+            </div>
+
+            <section className='admin-section'>
+                <div className='panel'>
+                    <div className='admin-name'>
+                        <h3>NAVNEET KUMAR</h3>
+                    </div>
+                    <div className='panel-items'>
+                        <p onClick={() => setCickedBtn(1)} className={clickedBtn === 1 ? 'selected' : ''}>Profile</p>
+                        <p onClick={() => setCickedBtn(2)} className={clickedBtn === 2 ? 'selected' : ''}>Users</p>
+                        <p onClick={() => setCickedBtn(3)} className={clickedBtn === 3 ? 'selected' : ''}>Collections</p>
+                        <p onClick={() => setCickedBtn(4)} className={clickedBtn === 4 ? 'selected' : ''}>Products</p>
+                        <p onClick={() => setCickedBtn(5)} className={clickedBtn === 5 ? 'selected' : ''}>Orders</p>
+                    </div>
+                </div>
+
+                <div className='panel-contents'>
+                    <div className='panel-data'>
+                        {clickedBtn === 1 && <AdminProfile/>}
+                        {clickedBtn === 2 && <AdminUsers/>}
+                        {clickedBtn === 3 && <AdminCollections/>}
+                        {clickedBtn === 4 && <AdminProducts/>}
+                        {clickedBtn === 5 && <AdminOrders/>}
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default AdminPage;
